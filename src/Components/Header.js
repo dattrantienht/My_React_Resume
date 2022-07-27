e2dde4feae95c08acaee1e79e9b53b23ff1841ce
Tran Tien Dat <74689952+dattrantienht@users.noreply.github.com>
2022-07-27 13:04:40 +0700

    update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ParticlesBg from "particles-bg";
 import Fade from "react-reveal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class Header extends Component {
